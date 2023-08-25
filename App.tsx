795f6a2038e032b5f2bae83f7546e54ed413495f
Andrea Da Silva <adasilvapdev@gmail.com>
2023-08-25 14:12:54 -0400

    initial commit :)

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+
+export default App;
